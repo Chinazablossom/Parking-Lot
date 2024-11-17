3f9ec02a71bad398426055aa4cb8c76c46310efa
chinazablossom <chinazablossom7@gmail.com>
2024-11-17 01:30:34 +0100

    Stage 1

---
+++ Parking Lot/task/src/parking/Main.kt
@@ -0,0 +1,8 @@
+package parking
+
+fun main() {
+    val words = listOf(" car has parked.", " car left the parking lot." , " car just parked here.")
+    println(readln()+words[0])
+    println(readln()+words[1])
+    println(readln()+words[2])
+}
